043b25204dc9cdf6fbfa22595a2f19d2ea07418b
Omer Haimovich <omer16493@gmail.com>
2018-06-04 09:54:21 +0300

    Add exam - initial

---
+++ AES_G4/src/root/client/controllers/AddExamController.java
@@ -0,0 +1,11 @@
+package root.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class AddExamController {
+
+    @FXML
+    private AnchorPane rootPane;
+
+}
