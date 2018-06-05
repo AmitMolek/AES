99effee6818799398f3e1adc7df7b38632a3c772
gal <gal@192.168.1.2>
2018-06-05 20:13:02 +0300

    mergin mallok work

--- AES_G4/src/root/client/Main.java
+++ AES_G4/src/root/client/Main.java
@@ -1,14 +1,10 @@
 package root.client;
 	
 
-<<<<<<< HEAD
-import root.client.managers.ScreensManager;
-=======
-import java.io.IOException;
-
 import javafx.scene.text.Font;
-import root.client.resources.view.ScreensManager;
->>>>>>> refs/remotes/origin/Amit
+//<<<<<<< HEAD
+import root.client.managers.ScreensManager;
+//>>>>>>> refs/remotes/origin/Amit
 import root.util.log.Log;
 import root.util.log.LogLine.LineType;
 
@@ -19,18 +15,18 @@ public class Main extends ScreensManager {
 	
 	public static void main(String[] args) {
 		Log.getInstance().writeToLog(LineType.INFO, "Application started", false);
-<<<<<<< HEAD
-		ScreensManager.addScreen("loginScreen","resources/view/LoginScreen.fxml");
-		ScreensManager.addScreen("mainWindow","resources/view/AddExamScreen.fxml");
-		ScreensManager.addScreen("questions","resources/view/Questions.fxml");	// questions
-=======
+//<<<<<<< HEAD
+//		ScreensManager.addScreen("loginScreen","resources/view/LoginScreen.fxml");
+//		ScreensManager.addScreen("mainWindow","resources/view/AddExamScreen.fxml");
+//		ScreensManager.addScreen("questions","resources/view/Questions.fxml");	// questions
+//=======
 		
 		Font.loadFont(Main.class.getResource("resources/fonts/Roboto-Regular.ttf").toExternalForm(), 12);
 		
 		ScreensManager.addScreen("main","resources/view/LoginScreen.fxml");
 		//ScreensManager.addScreen("mainWindow","resources/view/MainScreen.fxml");
 		ScreensManager.addScreen("home","resources/view/Home.fxml");
->>>>>>> refs/remotes/origin/Amit
+//>>>>>>> refs/remotes/origin/Amit
 		
 		launch(args);
 	}
--- AES_G4/src/root/client/controllers/LoginController.java
+++ AES_G4/src/root/client/controllers/LoginController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package root.client.controllers;
 
 import java.io.IOException;
@@ -18,6 +18,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 import ocsf.client.ObservableClient;
+import root.client.managers.DataKeepManager;
 import root.client.managers.LoggedInUserManager;
 import root.client.managers.ScreensManager;
 import root.dao.app.LoginInfo;
@@ -110,7 +111,7 @@ public class LoginController implements Observer {
 		if(arg1 instanceof UserMessage) {
 			UserMessage newMessasge = (UserMessage) arg1;
 			user = newMessasge.getUser();
-			loggedInManager.addUser(user);
+			DataKeepManager.getInstance().keepObject("user", user);
 			//System.out.println(user);
 			System.out.println("Logged In Users: "+ loggedInManager);
 			Platform.runLater(() -> {				// In order to run javaFX thread.(we recieve from server a java thread)
@@ -132,121 +133,121 @@ public class LoginController implements Observer {
 	}
 	
 }
-=======
-package root.client.controllers;
+//=======
+//package root.client.controllers;
+//
+//import java.io.IOException;
+//import java.util.Observable;
+//import java.util.Observer;
+//
+//import javafx.application.Platform;
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Hyperlink;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.PasswordField;
+//import javafx.scene.control.TextField;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.HBox;
+//import javafx.scene.layout.StackPane;
+//import javafx.scene.layout.VBox;
+//import ocsf.client.ObservableClient;
+//import root.client.managers.DataKeepManager;
+//import root.client.resources.view.ScreensManager;
+//import root.dao.app.LoginInfo;
+//import root.dao.app.User;
+//import root.dao.message.LoginMessage;
+//import root.dao.message.MessageFactory;
+//import root.dao.message.UserMessage;
+//
+//public class LoginController implements Observer {
+//
+//    @FXML
+//    private AnchorPane rootPane;
+//
+//    @FXML
+//    private Hyperlink linkForgot;
+//
+//    @FXML
+//    private Button btnSignIn;
+//
+//    @FXML
+//    private Label lblId;
+//
+//    @FXML
+//    private TextField txtId;
+//
+//    @FXML
+//    private Label lblPassword;
+//
+//    @FXML
+//    private PasswordField txtPassword;
+//
+//    
+//    private ObservableClient client;
+//    private MessageFactory message;
+//    private User user;
+//    private ScreensManager screenManager;
+//    /**
+//     * This method occurs when someone presses the sign in button
+//     * @param event action event when someone presses the sign in button
+//     */
+//    @FXML
+//    public void SignIn(ActionEvent event) {
+//    	String userId = txtId.getText();
+//    	String userPassword = txtPassword.getText();
+//    	LoginInfo loginInformation = new LoginInfo(userId,userPassword);
+//    	LoginMessage newLoginMessage = (LoginMessage) message.getMessage("login",loginInformation);
+//    	try {
+//			client.sendToServer(newLoginMessage);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//    	
+//
+//    }
+//    
+//    /**
+//     * This method occurs when the window is shown up.
+//     * @throws IOException if the window cannot be shown
+//     */
+//    @FXML
+//	public void initialize() throws IOException{
+//    	Platform.runLater(() -> rootPane.requestFocus());
+//    	message = MessageFactory.getInstance();
+//    	screenManager = ScreensManager.getInstance();
+//    	client = new ObservableClient("localhost", 8000);
+//    	client.addObserver(this);
+//    	client.openConnection();
+//    	
+//    	
+//    }
+//    
+//    /**
+//     * This method occurs when the server send message to the client
+//     */
+//	@Override
+//	public void update(Observable arg0, Object arg1) {
+//		System.out.println(arg1);
+//		
+//		if(arg1 instanceof UserMessage) {
+//			UserMessage newMessasge = (UserMessage) arg1;
+//			user = newMessasge.getUser();
+//			DataKeepManager.getInstance().keepObject("user", user);
+//			try {
+//				screenManager.activate("mainWindow");
+//
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+//    
+//    
+//
+//}
 
-import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
-
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import ocsf.client.ObservableClient;
-import root.client.managers.DataKeepManager;
-import root.client.resources.view.ScreensManager;
-import root.dao.app.LoginInfo;
-import root.dao.app.User;
-import root.dao.message.LoginMessage;
-import root.dao.message.MessageFactory;
-import root.dao.message.UserMessage;
-
-public class LoginController implements Observer {
-
-    @FXML
-    private AnchorPane rootPane;
-
-    @FXML
-    private Hyperlink linkForgot;
-
-    @FXML
-    private Button btnSignIn;
-
-    @FXML
-    private Label lblId;
-
-    @FXML
-    private TextField txtId;
-
-    @FXML
-    private Label lblPassword;
-
-    @FXML
-    private PasswordField txtPassword;
-
-    
-    private ObservableClient client;
-    private MessageFactory message;
-    private User user;
-    private ScreensManager screenManager;
-    /**
-     * This method occurs when someone presses the sign in button
-     * @param event action event when someone presses the sign in button
-     */
-    @FXML
-    public void SignIn(ActionEvent event) {
-    	String userId = txtId.getText();
-    	String userPassword = txtPassword.getText();
-    	LoginInfo loginInformation = new LoginInfo(userId,userPassword);
-    	LoginMessage newLoginMessage = (LoginMessage) message.getMessage("login",loginInformation);
-    	try {
-			client.sendToServer(newLoginMessage);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    	
-
-    }
-    
-    /**
-     * This method occurs when the window is shown up.
-     * @throws IOException if the window cannot be shown
-     */
-    @FXML
-	public void initialize() throws IOException{
-    	Platform.runLater(() -> rootPane.requestFocus());
-    	message = MessageFactory.getInstance();
-    	screenManager = ScreensManager.getInstance();
-    	client = new ObservableClient("localhost", 8000);
-    	client.addObserver(this);
-    	client.openConnection();
-    	
-    	
-    }
-    
-    /**
-     * This method occurs when the server send message to the client
-     */
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		System.out.println(arg1);
-		
-		if(arg1 instanceof UserMessage) {
-			UserMessage newMessasge = (UserMessage) arg1;
-			user = newMessasge.getUser();
-			DataKeepManager.getInstance().keepObject("user", user);
-			try {
-				screenManager.activate("mainWindow");
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-    
-    
-
-}
->>>>>>> refs/remotes/origin/Amit
