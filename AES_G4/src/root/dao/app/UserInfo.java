e29d2db005106552eee483e0bca1937ab30981cc
gal <gal@192.168.1.2>
2018-06-08 19:56:50 +0300

    .

---
+++ AES_G4/src/root/dao/app/UserInfo.java
@@ -0,0 +1,52 @@
+package root.dao.app;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserInfo implements Serializable{
+	/**
+	 * @author gal
+	 * this DAO class is for transferring data between client and server,
+	 * when we want to get all teacher name's who wrote the question's
+	 */
+	private HashMap<String, String> teachersMap;	// key = user ID, value = User fullName
+	private ArrayList<Question> questions;
+	private User userInfo;
+	/**
+	 * this constructor is called when we have users ID (e.g in questions) and we want to get their names;
+	 * @param teachersMap
+	 * @param questions
+	 */
+	public UserInfo(HashMap<String, String> teachersMap, ArrayList<Question> questions) {
+		super();
+		this.teachersMap = teachersMap;
+		this.questions = questions;
+	}
+	/**
+	 * @return the teachersMap
+	 */
+	public HashMap<String, String> getTeachersMap() {
+		return teachersMap;
+	}
+	/**
+	 * @param teachersMap the teachersMap to set
+	 */
+	public void setTeachersMap(HashMap<String, String> teachersMap) {
+		this.teachersMap = teachersMap;
+	}
+	/**
+	 * @return the questions
+	 */
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+	/**
+	 * @param questions the questions to set
+	 */
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+}
