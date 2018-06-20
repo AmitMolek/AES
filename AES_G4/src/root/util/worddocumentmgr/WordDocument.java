e3c2cdd5d7c838b6d69b130558e012daa8dd8386
Omer Haimovich <omer16493@gmail.com>
2018-06-20 11:02:43 +0300

    word document java docs

--- AES_G4/src/root/util/worddocumentmgr/WordDocument.java
+++ AES_G4/src/root/util/worddocumentmgr/WordDocument.java
@@ -16,21 +16,48 @@ import root.dao.app.Question;
 import root.dao.app.QuestionInExam;
 import root.server.managers.ServerMessageManager;
 
+/**
+ * The WordDocument is a class that is responsible for creating a new word file
+ * for manually exam
+ * 
+ * @author Omer Haimovich
+ *
+ */
 public class WordDocument {
 
+	// Instance variables **********************************************
+
+	/**
+	 * 
+	 * The manually exam that we want to do to him word document
+	 */
 	private Exam exam;
-	
+
+	// CONSTRUCTORS *****************************************************
+
+	/**
+	 * Constructs the WordDocument
+	 * 
+	 * @param exam
+	 *            The manually exam
+	 */
 	public WordDocument(Exam exam) {
 		super();
 		this.exam = exam;
 	}
 
+	/**
+	 * 
+	 * Method that is responsible for creating a new Word file that will contain all
+	 * the data of the exam includes it questions, 4 answers, scoring for each
+	 * question etc
+	 */
 	public void createDocument() {
 		XWPFDocument document = new XWPFDocument();
-		int i =0;
+		int i = 0;
 		try {
-			
-			File file = new File(ServerMessageManager.PATH + exam.getExamId()+ ".docx");
+
+			File file = new File(ServerMessageManager.PATH + exam.getExamId() + ".docx");
 			FileOutputStream out = new FileOutputStream(file);
 			// create Paragraph
 			XWPFParagraph paragraph = document.createParagraph();
@@ -44,19 +71,19 @@ public class WordDocument {
 			run.addBreak();
 			run.addBreak();
 			ArrayList<QuestionInExam> questionInExam = exam.getExamQuestions();
-			for(QuestionInExam q: questionInExam) {
+			for (QuestionInExam q : questionInExam) {
 				i++;
 				Question question = q.getQuestion();
-				run.setText("Question number "+ i +  ": " + question.getIdquestionIntruction());
+				run.setText("Question number " + i + ": " + question.getIdquestionIntruction());
 				run.addTab();
-				run.setText("(" + q.getQuestionGrade() +" points)");
+				run.setText("(" + q.getQuestionGrade() + " points)");
 				run.addBreak();
 				run.addBreak();
 				run.setText(question.getQuestionText());
 				run.addBreak();
-				if(q.getFreeTextForStudent() != null) {
-				run.setText("note: " + q.getFreeTextForStudent());
-				run.addBreak();
+				if (q.getFreeTextForStudent() != null) {
+					run.setText("note: " + q.getFreeTextForStudent());
+					run.addBreak();
 				}
 				run.addBreak();
 				run.setText("The answers are:");
@@ -75,7 +102,7 @@ public class WordDocument {
 				run.addBreak();
 				run.addBreak();
 				run.addBreak();
-				run.addBreak();		
+				run.addBreak();
 			}
 			run.setText("Good luck!");
 			document.write(out);
