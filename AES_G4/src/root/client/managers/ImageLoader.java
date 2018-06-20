f3dfa5e574e29ce3df037cfb0f61637ea2c7c1ef
AMIT\Amit Molek <iamitmolek@gmail.com>
2018-06-20 17:22:22 +0300

    fixed some shit and shit code from malloc :)

--- AES_G4/src/root/client/managers/ImageLoader.java
+++ AES_G4/src/root/client/managers/ImageLoader.java
@@ -1,9 +1,25 @@
 package root.client.managers;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.CodeSource;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
+
+import com.sun.javafx.iio.ImageStorage;
 
 import javafx.scene.image.Image;
+import root.client.Main;
 import root.util.log.Log;
 import root.util.log.LogLine.LineType;
 
@@ -23,7 +39,7 @@ public class ImageLoader {
 	 * @throws Exception throws an exception if the file could not been found
 	 */
 	public static Image loadImage(String filePath, String fileName) throws Exception{
-		File imgFile = new File(filePath + "/" + fileName);
+		File imgFile = new File(Main.class.getResource(filePath + "/" + fileName).toURI());
 		if (!imgFile.exists()) {
 			logError("Image could not be found");
 			return null;
@@ -39,21 +55,59 @@ public class ImageLoader {
 	 * @throws Exception
 	 */
 	public static ArrayList<Image> loadImagesFromFolder(String folderPath) throws Exception{
-		File folder = new File(folderPath);
-		if (!folder.isDirectory()) {
-			logError("Folder could not be found");
-			return null;
-		}else {
-			ArrayList<Image> images = new ArrayList<Image>();
-			File[] filesInFolder = folder.listFiles();
-			for (File file : filesInFolder) {
-				if (file.exists()) {
-					images.add(new Image(file.toURI().toString()));
-				}
+		List<URL> urls = getResources(folderPath);
+		ArrayList<Image> images = new ArrayList<>();
+		
+		for (URL url : urls) {
+			try {
+				InputStream is = url.openStream();
+				Image img = new Image(is);
+				System.out.println("Images: " + img.getHeight());
+				images.add(img);
+			}catch (IOException e) {
+				e.printStackTrace();
+				logError(e.getMessage());
 			}
-			return images;
 		}
+		return images;
 	}
+	
+	/**
+	 * Collects all the resources (in our case images) from the folder and creates a URL for each one of them
+	 * Then returns the collection of all the URLs
+	 * @param path the path of the folder
+	 * @return List of files URLs inside path folder
+	 * @throws IOException
+	 */
+	private static List<URL> getResources(final String path) throws IOException {
+		List<URL> urls = new ArrayList<>();
+		CodeSource src = Main.class.getProtectionDomain().getCodeSource();
+		if (src != null) {
+		  URL jar = src.getLocation();
+		  ZipInputStream zip = new ZipInputStream(jar.openStream());
+		  int firstIndex = 0;
+		  for (ZipEntry e = zip.getNextEntry(); e != null; e = zip.getNextEntry()) {
+			  String eName = e.getName();
+			  if (eName.startsWith(path)) {
+				  firstIndex++;
+				  if (firstIndex != 1) {
+					  System.out.println(Main.class.getResource("/" + eName));
+					  URL imgUrl = Main.class.getResource("/" + eName);
+					  urls.add(imgUrl);
+				  }
+			  }
+		  }
+		  return urls;
+
+		} 
+		else {
+			InputStream is = Main.class.getResourceAsStream("/root/client/resources/view");
+	        InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+	        BufferedReader br = new BufferedReader(isr);
+	        
+	        return br.lines().map(l -> path + "/" + l).map(r -> Main.class.getResource(r)).collect(Collectors.toList());
+		}
+	} 
 	
 	/**
 	 * Logs an error to the log and throws an exception
