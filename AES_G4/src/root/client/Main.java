afdac2a12c09f1fe88d0bf05e65692ca5d1712af
Omer Haimovich <omer16493@gmail.com>
2018-06-20 14:10:05 +0300

    more java docs - dao messages

--- AES_G4/src/root/client/Main.java
+++ AES_G4/src/root/client/Main.java
@@ -8,20 +8,27 @@ import root.client.managers.ScreensManager;
 import root.util.log.Log;
 import root.util.log.LogLine.LineType;
 
+/**
+ * 
+ * 
+ * A main class responsible for updating the manager responsible for
+ * switching between the windows and running the application
+ * 
+ * @author Amit Molek
+ *
+ */
 public class Main extends ScreensManager {
-    
-   
-    
+
 	public static void main(String[] args) {
 		Log log = Log.getInstance();
 		DataKeepManager dkm = DataKeepManager.getInstance();
 		log.writeToLog(LineType.INFO, "Application started", false);
-		ScreensManager.addScreen("solvedExams","resources/view/ViewSolvedExams.fxml");
-		
+		ScreensManager.addScreen("solvedExams", "resources/view/ViewSolvedExams.fxml");
+
 		dkm.keepObject("solvedExams Observer", new SolvedExamsController());
-		ScreensManager.addScreen("questions","resources/view/Questions.fxml");		
-		ScreensManager.addScreen("main","resources/view/LoginScreen.fxml");
-		ScreensManager.addScreen("home","resources/view/Home.fxml");
+		ScreensManager.addScreen("questions", "resources/view/Questions.fxml");
+		ScreensManager.addScreen("main", "resources/view/LoginScreen.fxml");
+		ScreensManager.addScreen("home", "resources/view/Home.fxml");
 		ScreensManager.addScreen("addExam", "resources/view/AddExamScreen.fxml");
 		ScreensManager.addScreen("updateDeleteExam", "resources/view/DeleteExamScreen.fxml");
 		ScreensManager.addScreen("executefull", "resources/view/ExecuteExamScreen.fxml");
@@ -30,15 +37,15 @@ public class Main extends ScreensManager {
 		ScreensManager.addScreen("check", "resources/view/check.fxml");
 		ScreensManager.addScreen("histograms", "resources/view/HistorgramsScreen.fxml");
 		ScreensManager.addScreen("executeExamScreen", "resources/view/ExecuteExamScreen.fxml");
-		ScreensManager.addScreen("PrepareExam", "resources/view/PerpareExamScreen.fxml");		
-		ScreensManager.addScreen("manuallyExam", "resources/view/ManuallyExamScreen.fxml");
-		ScreensManager.addScreen("endExam", "resources/view/ExamSubmitScreen.fxml");	
 		ScreensManager.addScreen("PrepareExam", "resources/view/PerpareExamScreen.fxml");
-		ScreensManager.addScreen("changeDurations", "resources/view/ChangeDurationTimeScreen.fxml");	
+		ScreensManager.addScreen("manuallyExam", "resources/view/ManuallyExamScreen.fxml");
+		ScreensManager.addScreen("endExam", "resources/view/ExamSubmitScreen.fxml");
+		ScreensManager.addScreen("PrepareExam", "resources/view/PerpareExamScreen.fxml");
+		ScreensManager.addScreen("changeDurations", "resources/view/ChangeDurationTimeScreen.fxml");
 		ScreensManager.addScreen("testTeacherGradesStats", "resources/view/TestGradesTeacher.fxml");
 		ScreensManager.addScreen("testPrincipalGradesStats", "resources/view/TestGradesPrincipal.fxml");
 		ScreensManager.addScreen("teacherExamsView", "resources/view/TeacherExamsView.fxml");
 		launch(args);
 	}
-	
+
 }
--- AES_G4/src/root/dao/message/SimpleMessage.java
+++ AES_G4/src/root/dao/message/SimpleMessage.java
@@ -2,17 +2,43 @@ package root.dao.message;
 
 import javafx.scene.Parent;
 
+/**
+ * A class that transmits messages between the server and the client with regard
+ * to simple message information
+ * 
+ * @author Omer Haimovich
+ *
+ */
 public class SimpleMessage extends AbstractMessage {
 
+	// CONSTRUCTORS *****************************************************
+
+	/**
+	 * Constructs the SimpleMessage
+	 * 
+	 * @param string
+	 *            the message that you want to transmit to the server or client
+	 */
 	public SimpleMessage(String string) {
 		super(string);
 	}
 
+	/**
+	 * 
+	 * Returns the type of message
+	 */
 	@Override
 	public String getType() {
 		return "SimpleMessage";
 	}
-	
+
+	/**
+	 * A method that set the message that you want to transmit to the server or
+	 * client
+	 * 
+	 * @param message
+	 *            the message that you want to transmit to the server or client
+	 */
 	public void setMessage(String message) {
 		super.setMsg(message);
 	}
