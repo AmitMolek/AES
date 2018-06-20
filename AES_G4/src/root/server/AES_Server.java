29d420b1d8cd762a977832a0ee5f64ecaadce516
Omer Haimovich <omer16493@gmail.com>
2018-06-20 10:39:12 +0300

    AES_server java docs

--- AES_G4/src/root/server/AES_Server.java
+++ AES_G4/src/root/server/AES_Server.java
@@ -24,41 +24,75 @@ import root.util.log.LogLine;
 /**
  * 
  * This Class is implements the server side The class extends the AbstractServer
- * class Super class is implemented all the connection methods
- * The connection with the server is used with message class.
+ * class Super class is implemented all the connection methods The connection
+ * with the server is used with message class.
  */
 public class AES_Server extends AbstractServer {
 
+	// Instance variables **********************************************
+	
+	/**
+	 * 
+	 * Default port where the server is going to run
+	 */
 	final public static int DEFAULT_PORT = 8000;
+	/**
+	 * 
+	 * Responsible for handle with messages sent to the server from the client
+	 */
 	private ServerMessageManager smm;
+	/**
+	 * 
+	 * Generates new messages that link the server to the client
+	 */
 	private MessageFactory msgFactory;
-	String[] recivedMSG;
-	private ArrayList<Question> dataBase;
+	/**
+	 * 
+	 * Connection between the database and the server
+	 */
 	private static Connection conn;
+	/**
+	 * 
+	 * A log file that is responsible for documenting the actions performed in the
+	 * application
+	 */
 	private static Log log;
+	/**
+	 * 
+	 */
 	public static ConnectionToClient CLIENT;
+
+	// CONSTRUCTORS *****************************************************
+	
+	/**
+	 * Constructs the AES_Server
+	 * @param port the port where the server is going to run
+	 */
 	public AES_Server(int port) {
 		super(port);
-		smm=ServerMessageManager.getInstance();
-		msgFactory=MessageFactory.getInstance();
+		smm = ServerMessageManager.getInstance();
+		msgFactory = MessageFactory.getInstance();
 		log = Log.getInstance();
 	}
+
+	// CLASS methods ************************************************
+	
 	/**
 	 *  Get the server connection
+	 * @return the server connection
 	 */
 	public static Connection getConnection() {
 		return conn;
 	}
-	// Instance methods ************************************************
-
+	
 	/**
-	 * This method handles any messages received from the client.
+	 * This method handles any messages received from the client.s
 	 */
 	public void handleMessageFromClient(Object msg, ConnectionToClient client) {
 		this.CLIENT = client;
-		if(msg instanceof AbstractMessage) {
+		if (msg instanceof AbstractMessage) {
 			AbstractMessage msgToHandle = (AbstractMessage) msg;
-			AbstractMessage msgToReturn=smm.handleMessage(msgToHandle);
+			AbstractMessage msgToReturn = smm.handleMessage(msgToHandle);
 			try {
 				client.sendToClient(msgToReturn);
 			} catch (IOException e) {
@@ -88,8 +122,8 @@ public class AES_Server extends AbstractServer {
 	// Class methods ***************************************************
 
 	/**
-	 * The main method creates mysql driver
-	 * The main method creates connection with the client
+	 * The main method creates mysql driver The main method creates connection with
+	 * the client
 	 * 
 	 */
 	public static void main(String[] args) {
@@ -103,11 +137,9 @@ public class AES_Server extends AbstractServer {
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/aes", "root", "204403257");
 
-
-
 			System.out.println("SQL connection succeed");
 		} catch (SQLException ex) {/* handle any errors */
-			//log.writeToLog(LogLine.LineType.ERROR, ex.getMessage());
+			// log.writeToLog(LogLine.LineType.ERROR, ex.getMessage());
 			System.out.println("SQLException: " + ex.getMessage());
 			System.out.println("SQLState: " + ex.getSQLState());
 			System.out.println("VendorError: " + ex.getErrorCode());
@@ -118,7 +150,7 @@ public class AES_Server extends AbstractServer {
 		} catch (Throwable t) {
 			port = DEFAULT_PORT;
 		}
-		
+
 		AES_Server sv = new AES_Server(port);
 		log.writeToLog(LogLine.LineType.INFO, "The server is connected");
 
