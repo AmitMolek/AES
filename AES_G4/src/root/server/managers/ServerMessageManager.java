3dab9d2c79a29f55a8de5adb5a8bfa73159e48e3
Omer Haimovich <omer16493@gmail.com>
2018-06-22 12:12:02 +0300

    fix bugs

--- AES_G4/src/root/server/managers/ServerMessageManager.java
+++ AES_G4/src/root/server/managers/ServerMessageManager.java
@@ -91,11 +91,11 @@ public class ServerMessageManager {
 	// Instance variables **********************************************
 
 	/**
-	 * 
+	 * Manager for of all students currently under exam
 	 */
 	private static ExecuteStudentManager examinees = ExecuteStudentManager.getInstance();
 	/**
-	 * 
+	 * Manager for of all principal currently logged in 
 	 */
 	private static PrincipleManager principles = new PrincipleManager();
 	/**
@@ -107,7 +107,7 @@ public class ServerMessageManager {
 	 */
 	private static MessageFactory message = MessageFactory.getInstance();
 	/**
-	 * 
+	 * Manager all users currently logged in
 	 */
 	private static LoggedInUsersManager usersManager = LoggedInUsersManager.getInstance();
 	/**
@@ -128,7 +128,7 @@ public class ServerMessageManager {
 	 */
 	public static String PATHCSV;
 	/**
-	 * 
+	 * Manager for of all students currently under exam
 	 */
 	public static ExamExecutedManager executedUsersManager = new ExamExecutedManager();
 
